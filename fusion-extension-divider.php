3548b909d69854ba71ebb40a9486b14eb0c7712e
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 14:06:02 -0300

    Update plugin URL

--- fusion-extension-divider.php
+++ fusion-extension-divider.php
@@ -4,7 +4,7 @@
  */
 /**
  * Plugin Name: Fusion : Extension - Divider
- * Plugin URI: http://fusion.1867dev.com
+ * Plugin URI: http://www.agencydominion.com/fusion/
  * Description: Divider Extension Package for Fusion.
  * Version: 1.1.0
  * Author: Agency Dominion
